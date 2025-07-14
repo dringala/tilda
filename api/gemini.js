e630716407c9d1a1915663ee63cdda48de221d2d
Alex Dring <alex.dring@AngelSolutionsLtd.onmicrosoft.com>
2025-07-14 13:57:04 +0100

    update key name

--- api/gemini.js
+++ api/gemini.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method Not Allowed. Only POST requests are supported.' });
   }
 
-  const apiKey = process.env.GEMINI_API_KEY;
+  const apiKey = process.env.GOOGLE_GENERATIVE_AI_API_KEY ;
 
   let prompt;
   try {
